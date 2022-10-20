fecf51d519df4510e0ecc9fb5d2c226e9c192440
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-10-20 14:16:24 -0300

    Commit inicial

--- App.tsx
+++ App.tsx
@@ -1,17 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const WEBSITE = 'http://192.168.0.108:5173/'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ width: '100%', height: '100%' }}>
+        <WebView
+          source={{ uri: WEBSITE }}
+          onLoad={() => console.log('loaded')}
+        />
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Platform.OS === 'android' ? 35 : 50 ,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
